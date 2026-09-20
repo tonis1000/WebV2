51b67482b6a3ddf0d72c69101b1caa0b1c0f770c
tonis1000 <atonis@live.com>
2026-09-20 18:23:16 +0200

    Migrate V2 config

---
+++ src/config.js
@@ -0,0 +1,63 @@
+export const CONFIG = Object.freeze({
+  appName: 'WebTV V2',
+  buildId: '20260920-1021',
+  cacheBaseUrl: 'https://tv-cache.atonis.workers.dev',
+  epgUrl: 'https://epg-proxy-gr.atonis.workers.dev/epg.xml',
+  channelCatalogUrl: './data/channels.m3u?v=20260920-1021',
+  healthStorageKey: 'webtv_v2_health',
+  requestTimeoutMs: 9000,
+  startupTimeoutMs: 12000,
+  epgRefreshMs: 30 * 60 * 1000,
+  healthMaxAgeMs: 30 * 24 * 60 * 60 * 1000,
+  failureCooldownThreshold: 2,
+  failureCooldownBaseMs: 15 * 60 * 1000,
+  failureCooldownMaxMs: 6 * 60 * 60 * 1000,
+  workerForHls: true,
+  maxNextPrograms: 3,
+});
+
+export const OFFICIAL_LIVE = Object.freeze({
+  ert1: 'https://live.ertflix.gr/',
+  ert2: 'https://live.ertflix.gr/',
+  ert3: 'https://live.ertflix.gr/',
+  ertnews: 'https://live.ertflix.gr/',
+  ant1: 'https://www.antenna.gr/live',
+});
+
+export const SOURCE_BLOCKLIST = Object.freeze([
+  'https://spark3.smart-tv-data.com/ert1HD/ert1HD/playlist.m3u8',
+  'https://cdn4.smart-tv-data.com/vid/ert1/playlist.m3u8',
+  'http://195.226.218.163/vid/ert1/playlist.m3u8',
+  'https://ert-live.siliconweb.com/media/ert_1/ert_1.m3u8',
+  'https://ertflix.s.llnwi.net/ertlive/ert1/clrdef24723b/playlist.m3u8',
+  'https://ert-ucdn.broadpeak-aas.com/bpk-tv/ERT1/default/index.m3u8',
+  'https://spark3.smart-tv-data.com/vid/ert2hd/playlist.m3u8',
+  'https://wow.anixa.tv/live/ert2/playlist.m3u8',
+  'https://ert-live.siliconweb.com/media/ert_2/ert_2.m3u8',
+  'https://ertflix.s.llnwi.net/ertlive/ert2/clrdef24828z/playlist.m3u8',
+  'https://ert-ucdn.broadpeak-aas.com/bpk-tv/ERT2/default/index.m3u8',
+  'https://spark3.smart-tv-data.com/vid/ert3hd/playlist.m3u8',
+  'https://ertflix.akamaized.net/ertlive/ert3/clrdef24828n/playlist.m3u8',
+  'https://ertflix.s.llnwi.net/ertlive/ertnews/default/index.m3u8',
+  'https://ertflix.akamaized.net/ertlive/ertnews/default/playlist.m3u8',
+  'https://ert-live.siliconweb.com/media/ert_news/ert_news.m3u8',
+  'https://lcdn.antennaplus.gr/r86d08d448885424196f6cd3ddc5d1489/eu-central-1/6415884360001/playlist_dvr.m3u8',
+  'https://spark3.smart-tv-data.com/ant1HD/ant1HD/playlist.m3u8',
+  'https://mcdn.antennaplus.gr/live/media0/Ant1/HLS/Ant1.m3u8',
+  'https://cdn1.smart-tv-data.com/live/ant1/playlist.m3u8',
+]);
+
+export const CHANNEL_ALIASES = Object.freeze({
+  ert1: ['ERT1.gr', 'ERT1.HD.gr', 'EPT1.gr', 'ΕΡΤ1'],
+  ert2: ['ERT2.gr', 'ERT2.HD.gr', 'EPT2.gr', 'ΕΡΤ2'],
+  ert3: ['ERT3.gr', 'ERT3.HD.gr', 'EPT3.gr', 'ΕΡΤ3'],
+  ertnews: ['ERTNEWS.gr', 'ERT.NEWS.gr', 'ΕΡΤNEWS'],
+  ant1: ['ANT1.gr', 'ANT1.HD.gr', 'Antenna1.gr'],
+  alpha: ['ALPHA.gr', 'ALPHA.HD.gr', 'Alpha.gr', 'Alpha.HD.gr', 'alphatv'],
+  skai: ['SKAI.gr', 'SKAI.HD.gr', 'skaitv'],
+  open: ['OPEN.gr', 'OPEN.HD.gr', 'OPEN.BEYOND.HD.gr', 'opentv'],
+  mega: ['MEGA.gr', 'MEGA.HD.gr', 'MegaChannel.gr', 'megatv'],
+  star: ['STAR.gr', 'STAR.HD.gr', 'startv'],
+  action24: ['ACTION24.gr', 'ACTION24.HD.gr'],
+  kontra: ['KONTRA.gr', 'KONTRA.HD.gr'],
+});
