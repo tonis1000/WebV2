57b0e6a30b6a13c4d3c3b82a40c1bbcf41f88e30
tonis1000 <atonis@live.com>
2026-09-25 19:08:46 +0200

    chore: refresh Xtream UI asset version

--- src/registry-default.js
+++ src/registry-default.js
@@ -6,4 +6,4 @@ try{
 
 // Xtream source UI is loaded here so index.html does not need another entry point.
 // It injects its card into Playlist Manager and reuses WebTVPlaylistAPI at click time.
-import './xtream-ui.js?v=20260925-xtream1';
+import './xtream-ui.js?v=20260925-xtream2';
