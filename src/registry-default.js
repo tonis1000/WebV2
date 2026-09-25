d9f112cdfda4ce22bcc4bd25da9c55d8baa18017
tonis1000 <atonis@live.com>
2026-09-25 23:55:45 +0200

    chore: bust Xtream enhancements cache

--- src/registry-default.js
+++ src/registry-default.js
@@ -8,4 +8,4 @@ try{
 // It injects its card into Playlist Manager and reuses WebTVPlaylistAPI at click time.
 import './xtream-ui.js?v=20260925-xtream6';
 import './xtream-preview-actions.js?v=20260925-xtream-click1';
-import './xtream-enhancements.js?v=20260925-xtream-enh1';
+import './xtream-enhancements.js?v=20260925-xtream-enh2';
